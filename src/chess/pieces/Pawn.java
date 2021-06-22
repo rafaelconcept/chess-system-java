faa40ac3cb09881a954324d94a783bada4762db9
rafaelconcept <rafaelconcept@hotmail.com>
2021-06-21 22:35:26 -0300

    ending pawn, fix the chackmate and put some pieces on board

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -11,6 +11,11 @@ public class Pawn extends ChessPiece {
 		super(board, color);
 		
 	}
+	
+	@Override
+	public String toString() {
+		return "P";
+	}
 
 	@Override
 	public boolean[][] possibleMoves() {
@@ -57,7 +62,7 @@ public class Pawn extends ChessPiece {
 			}
 		}
 		
-		return null;
+		return mat;
 	}
 
 }
